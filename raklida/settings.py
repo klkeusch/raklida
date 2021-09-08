0c2cf7bb94010468f72a538768102010019b64a6
K. Keusch <hallo@klauskeusch.de>
2021-09-08 08:37:24 +0200

    Added base for profile app: views, packet crispy_forms. Added register function with form.

--- raklida/settings.py
+++ raklida/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'profiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy forms config
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
