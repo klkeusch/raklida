fc320b69a75e39beb0daebfcf542dae6da76c470
K. Keusch <hallo@klauskeusch.de>
2021-09-08 16:44:06 +0200

    Fixed links and language in templates; Added CBVs for blog editing etc.

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,55 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-def list_blogs(request):
-    blogs = Blog.objects.all()
-    return render(request, "blog/list.html", {"blogs": blogs})
+class BlogListView(generic.ListView):
+    model = Blog
+    ordering = ['-date_published']
+    paginate_by = 3
 
 
-def detail_blog(request, pk):
-    blog = get_object_or_404(Blog, pk=pk)
-    return render(request, "blog/detail.html", {"blog": blog})
+class BlogDetailView(generic.DetailView):
+    model = Blog
+
+
+class BlogCreateView(LoginRequiredMixin, SuccessMessageMixin, generic.CreateView):
+    model = Blog
+    fields = ['title', 'content']
+    success_message = "Blogeintrag erfolgreich erstellt!"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Blog
+    fields = ['title', 'content']
+    success_message = "Blogeintrag erfolgreich aktualisiert!"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        blog = self.get_object()
+        if self.request.user == blog.author:
+            return True
+        else:
+            return False
+
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Blog
+    success_message = "Blog erfolgreich gelöscht!"
+    success_url = "/"
+
+    def test_func(self):
+        blog = self.get_object()
+        if self.request.user == blog.author:
+            return True
+        else:
+            return False
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView, self).delete(request, *args, **kwargs)
