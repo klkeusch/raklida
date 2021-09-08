1373507b392f2c07b7b16273f2aa6ea93f3eea9b
K. Keusch <hallo@klauskeusch.de>
2021-09-08 08:37:24 +0200

    Added base for profile app: views, packet crispy_forms. Added register function with form.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.views import generic
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def list_blogs(request):
     blogs = Blog.objects.all()
-    return get_object_or_404(request, "blog/list.html", {'blogs': blogs})
+    return render(request, "blog/list.html", {"blogs": blogs})
 
 
 def detail_blog(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
-    return render(request, "blog/detail.html", {'blog': blog})
+    return render(request, "blog/detail.html", {"blog": blog})
