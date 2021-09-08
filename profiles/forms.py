8da0bb4fa5abcb9ec55864f3e410fcab9310cf2b
K. Keusch <hallo@klauskeusch.de>
2021-09-08 15:38:44 +0200

    Fixed links and language in templates; Added profile update; Added email config, login redirect; Added password reset;

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
@@ -11,3 +12,22 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=50, required=True, label="Vorname")
+    last_name = forms.CharField(max_length=50, required=True, label="Nachname")
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'first_name', 'last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea, max_length=300, required=False, label="Informationen")
+    image = forms.ImageField(label="Profilbild")
+
+    class Meta:
+        model = Profile
+        fields = ['description', 'image']
