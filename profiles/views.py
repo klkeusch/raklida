1998ac9a6635034788ea3d65c861de9f88845697
K. Keusch <hallo@klauskeusch.de>
2021-09-08 09:29:57 +0200

    Added profile view; urls for login, logout, profile; Changed login redirect url in settings; Added templates for login, logout and profile;

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.models import User
 from .forms import RegisterForm
 
 
@@ -13,3 +14,8 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, "profiles/register.html", {'form': form})
+
+
+def profile(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, "profiles/profile.html", {'user': user})
