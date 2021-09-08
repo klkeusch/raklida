d53db7b60f47cf2cca5198cd9e32393a7cdc9767
K. Keusch <hallo@klauskeusch.de>
2021-09-08 16:44:06 +0200

    Fixed links and language in templates; Added CBVs for blog editing etc.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.list_blogs, name="blog_list"),
-    path("blog/<int:pk>", views.detail_blog, name="blog_detail"),
+    path("", views.BlogListView.as_view(), name="blog_list"),
+    path("blog/<int:pk>", views.BlogDetailView.as_view(), name="blog_detail"),
+    path("blog/create/", views.BlogCreateView.as_view(), name="blog_create"),
+    path("blog/<int:pk>/update", views.BlogUpdateView.as_view(), name="blog_update"),
+    path("blog/<int:pk>/delete", views.BlogDeleteView.as_view(), name="blog_delete"),
 ]
