f07bb014f9ec7672913378a0acb333d748d03ec9
K. Keusch <hallo@klauskeusch.de>
2021-09-08 07:21:16 +0200

    Added base for blogs app: model, views, templates. Updated urls.py. Created Stylesheet

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+content_validator = MinLengthValidator(limit_value=50, message="Der Bloginhalt sollte mindestens 50 Zeichen enthalten!")
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(validators=[content_validator])
+    date_published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={'pk': self.pk})
