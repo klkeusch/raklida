7e7c7617487a817e4307c8fd9855cf375ecf42e9
K. Keusch <hallo@klauskeusch.de>
2021-09-08 15:38:44 +0200

    Fixed links and language in templates; Added profile update; Added email config, login redirect; Added password reset;

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import RegisterForm
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -19,3 +20,23 @@ def register(request):
 def profile(request, pk):
     user = get_object_or_404(User, pk=pk)
     return render(request, "profiles/profile.html", {'user': user})
+
+
+@login_required
+def update(request):
+    if request.POST:
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form and profile_form:
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Profil erfolgreich aktualisiert!")
+            return redirect('profile', request.user.pk)
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'profiles/update.html', context)
--- raklida/urls.py
+++ raklida/urls.py
@@ -27,8 +27,22 @@ urlpatterns = [
     path("register/", profiles_views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="profiles/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="profiles/logout.html"), name="logout"),
-    # User related paths above
     path("profile/<int:pk>", profiles_views.profile, name="profile"),
+    path("profile/update", profiles_views.update, name="update"),
+    # User related paths above
+    # password reset below
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="profiles/password_reset.html"),
+         name="password_reset"),
+    path('password-reset/confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="profiles/password_reset_confirm.html"),
+         name="password_reset_confirm"),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name="profiles/password_reset_done.html"),
+         name="password_reset_done"),
+    path('password-reset/complete',
+         auth_views.PasswordResetCompleteView.as_view(template_name="profiles/password_reset_complete.html"),
+         name="password_reset_complete"),
+    # password reset above
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
