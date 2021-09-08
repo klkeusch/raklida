edec6579fe3db64e7d0b7fc66651c52732029140
K. Keusch <hallo@klauskeusch.de>
2021-09-08 21:50:08 +0200

    Added django_tables2 functionalities. Test with github example and Data model;
    Added Datapoints, Devices to models and admin; downsized display-3 to display-5 - much better; .landing 130px -> 50px; sensorvalues_list polished; Django_tables2 config added in settings.py

--- sensorvalues/models.py
+++ sensorvalues/models.py
@@ -8,7 +8,7 @@ class Data(models.Model):
     value_double = models.FloatField(blank=True, null=True)
     value_integer = models.BigIntegerField(blank=True, null=True)
     value_string = models.CharField(max_length=100, blank=True, null=True)
-    # datapoint = models.ForeignKey('Datapoints', models.DO_NOTHING)
+    datapoint = models.ForeignKey('Datapoints', models.DO_NOTHING)
     is_valid = models.BooleanField()
 
     class Meta:
@@ -16,7 +16,45 @@ class Data(models.Model):
         db_table = 'data'
 
     def __str__(self):
-        return self.id
+        return str(self.datapoint)
+
+
+class Datapoints(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    unit = models.CharField(max_length=15)
+    display_name = models.CharField(max_length=50)
+    store_historic_data = models.BooleanField()
+    device_sub_id = models.BigIntegerField()
+    device = models.ForeignKey('Devices', models.DO_NOTHING)
+    current_value_double = models.FloatField(blank=True, null=True)
+    current_value_integer = models.BigIntegerField(blank=True, null=True)
+    current_value_string = models.CharField(max_length=30, blank=True, null=True)
+    last_update = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'datapoints'
+
+    def __str__(self):
+        return self.display_name
+
+
+class Devices(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    location = models.CharField(max_length=30)
+    current_status_code = models.BigIntegerField()
+    last_status_update = models.TimeField()
+    device_type = models.CharField(max_length=30, blank=True, null=True)
+    display_name = models.CharField(max_length=50)
+    platform = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'devices'
+
+    def __str__(self):
+        return self.display_name
 
 
 class SimpleTable(tables.Table):
