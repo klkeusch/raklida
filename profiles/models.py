a8d0c65076d5f40806cc7fa2b40b399353f45deb
K. Keusch <hallo@klauskeusch.de>
2021-09-14 10:42:44 +0200

    Added plotly functionalities. Test with example;
    Added many-to-many-rel for devices<>profiles! Chose profiles because profiles are already connected to User-Model (good? we'll see...)

--- profiles/models.py
+++ profiles/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from sensorvalues.models import Devices
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pictures")
     description = models.CharField(max_length=300, blank=True)
+    ass_device = models.ManyToManyField(Devices)
 
     def __str__(self):
         return f'{self.user.username}\'s Profil...'
