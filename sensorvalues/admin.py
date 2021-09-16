db143c962e8932ebdbc611fa0e2db58a707216bb
K. Keusch <hallo@klauskeusch.de>
2021-09-16 17:26:49 +0200

    Added plotly functionalities. Test with example;
    Tested implementation of device assignments to user/profiles; added table for device<>user-assignments; to display these assignm. did not work

--- sensorvalues/admin.py
+++ sensorvalues/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Data, Datapoints, Devices, MqttTreeNodes, TreeDatapointTranslations, DevicesTable, DailyAverages
+from .models import Data, Datapoints, Devices, MqttTreeNodes, TreeDatapointTranslations, DevicesTable, DailyAverages, DeviceUserAssignment
 
 admin.site.register(Data)
 admin.site.register(Datapoints)
@@ -7,4 +7,5 @@ admin.site.register(Devices)
 admin.site.register(MqttTreeNodes)
 admin.site.register(TreeDatapointTranslations)
 admin.site.register(DailyAverages)
+admin.site.register(DeviceUserAssignment)
 
