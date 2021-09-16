adc091b51d2ccd2e317c3c9541404bb9386b7ff0
K. Keusch <hallo@klauskeusch.de>
2021-09-16 19:46:57 +0200

    Crashed database / migrations
    Setup new db, migrations: delete migrations folder in each app-folder; replace with blank migrations/__init__.py; makemigrations --> migrate

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+# admin.site.register(Profile)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('id', 'user', 'description',)
+
+
+admin.site.register(Profile, ProfileAdmin)
