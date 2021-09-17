972939719de51d882ac27f0d49ba0ca39644681b
K. Keusch <hallo@klauskeusch.de>
2021-09-17 09:40:27 +0200

    Fixed model views in admin; Added ./templates/admin/change_list.html to customize Django-Admin-Template (for plotly...)

--- sensorvalues/admin.py
+++ sensorvalues/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Data, Datapoints, Devices, MqttTreeNodes, TreeDatapointTranslations, DevicesTable, DailyAverages
+from .models import Data, Datapoints, Devices, MqttTreeNodes, TreeDatapointTranslations, DevicesTable, DailyAverages, \
+    DeviceUserAssignment
 
-admin.site.register(Datapoints)
+# admin.site.register(Datapoints)
 admin.site.register(MqttTreeNodes)
 admin.site.register(TreeDatapointTranslations)
 admin.site.register(DailyAverages)
@@ -12,16 +13,24 @@ class DevicesAdmin(admin.ModelAdmin):
     list_display = ('id', 'display_name', 'location', 'last_status_update',)
 
 
-#class DevicesUserAssignmentAdmin(admin.ModelAdmin):
-#    model = DeviceUserAssignment
-#    list_display = ('id', 'user', 'device',)
+class DevicesUserAssignmentAdmin(admin.ModelAdmin):
+    model = DeviceUserAssignment
+    list_display = ('id', 'user', 'device',)
 
 
 class DataAdmin(admin.ModelAdmin):
     model = Data
-    list_display = ('id', 'timestamp', 'value_double', 'value_integer', 'value_string', 'is_valid',)
+    list_display = ('id', 'timestamp', 'datapoint', 'value_double', 'value_integer', 'value_string', 'is_valid',)
+
+
+class DatapointAdmin(admin.ModelAdmin):
+    model = Datapoints
+    list_display = (
+    'id', 'display_name', 'unit', 'device', 'current_value_double', 'current_value_integer', 'current_value_string',
+    'last_update', 'store_historic_data')
 
 
 admin.site.register(Devices, DevicesAdmin)
 admin.site.register(Data, DataAdmin)
-#admin.site.register(DeviceUserAssignment, DevicesUserAssignmentAdmin)
+admin.site.register(DeviceUserAssignment, DevicesUserAssignmentAdmin)
+admin.site.register(Datapoints, DatapointAdmin)
