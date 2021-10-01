ed57c9d08fcf0c58d32f3922bde582c3af4700e2
K. Keusch <hallo@klauskeusch.de>
2021-10-01 10:59:08 +0200

    3 blank rows coming from: InlineModelAdmin.extra¶ This controls the number of extra forms the formset will display in addition to the initial forms. Defaults to 3. See the formsets documentation for more information.---> set to 0

--- profiles/admin.py
+++ profiles/admin.py
@@ -5,11 +5,12 @@ from sensorvalues.models import Devices
 
 class DevicesInline(admin.TabularInline):
     model = Devices
+    extra = 0
 
 
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
-    list_display = ('id', 'user', 'description', 'user_rooms')
+    list_display = ('id', 'user', 'description', 'user_rooms',)
     list_display_links = ('id', 'user')
     inlines = [
         DevicesInline,
