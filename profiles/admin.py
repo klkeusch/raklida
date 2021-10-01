b437836faeacef95868dc42bbf8b24ca32f80501
K. Keusch <hallo@klauskeusch.de>
2021-10-01 10:55:52 +0200

    Testing with django-admin chart (not working); implementing foreign keys in profile/devices for easy assignments (working with errors -> 3 blank rows every time added); Added clickable fields to profile django-admin with list_display_links = ('id', 'user'); deleted device-user-assignments-stuff to start fresh

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import Profile
+from sensorvalues.models import Devices
+
+
+class DevicesInline(admin.TabularInline):
+    model = Devices
 
 
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
     list_display = ('id', 'user', 'description', 'user_rooms')
-
+    list_display_links = ('id', 'user')
+    inlines = [
+        DevicesInline,
+    ]
 
 admin.site.register(Profile, ProfileAdmin)
