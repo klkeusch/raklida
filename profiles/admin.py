54a3622a704586c0b270e22b0c6bc49ce8fd1145
K. Keusch <hallo@klauskeusch.de>
2021-10-01 11:08:23 +0200

    StackedInline looks prettier; Added verbose names to clarify things; Added change link for devices

--- profiles/admin.py
+++ profiles/admin.py
@@ -3,9 +3,14 @@ from .models import Profile
 from sensorvalues.models import Devices
 
 
-class DevicesInline(admin.TabularInline):
+class DevicesInline(admin.StackedInline):
     model = Devices
     extra = 0
+    # raw_id_fields = ('in_rooms',)
+    verbose_name = 'Zugewiesenes Gerät'
+    verbose_name_plural = 'Zugewiesene Geräte'
+    fk_name = 'in_rooms'
+    show_change_link = True
 
 
 class ProfileAdmin(admin.ModelAdmin):
