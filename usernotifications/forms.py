7b6ce180d782c4acf0bf16d66bd29ac70bf5af3f
K. Keusch <hallo@klauskeusch.de>
2021-10-15 11:11:36 +0200

    Fixed user selection for assigned devices in message form

--- usernotifications/forms.py
+++ usernotifications/forms.py
@@ -9,8 +9,8 @@ class MessageCreateForm(forms.ModelForm):
     recipient = forms.ModelChoiceField(queryset=None, initial=0, label="Empfänger")
     content = forms.CharField(widget=forms.Textarea, max_length=500, required=True, label="Ihre Nachricht")
     incident_date = forms.DateTimeField(label="Zwischenfall-Datum")
-    user_devices = forms.ModelChoiceField(
-        queryset=None)  # forms.ModelMultipleChoiceField(queryset=None, label="Betroffenes Gerät")#(queryset=Devices.objects.all(), initial=0, label="Betroffenes Gerät")
+    user_devices = forms.ModelChoiceField(queryset=None, label="Betroffenes Gerät",
+                                          empty_label="Bitte Gerät auswählen...")
 
     class Meta:
         model = Message
@@ -22,17 +22,14 @@ class MessageCreateForm(forms.ModelForm):
         super(MessageCreateForm, self).__init__(*args, **kwargs)
 
         self.fields['recipient'].queryset = User.objects.all().exclude(
-            is_staff=False)  # Benutzer ist niemals Mitarbeiter
+            is_staff=False)
         self.fields['incident_date'].widget = DateTimePickerInput(options={
             "format": "DD.MM.YYYY HH:mm",
             "locale": "de",
         }, )
-        self.fields['user_devices'].queryset = DeviceUserAssignment.objects.filter(
-            device__profile__user=self.request.user
-        ).values_list(
-            "device__profile__assigned_devices__display_name",
-            flat=True
-        ).distinct()
+        self.fields['user_devices'].queryset = DeviceUserAssignment.objects.all().filter(
+            assigned_user__user=self.request.user
+        )
 
 
 class MessageUpdateForm(forms.ModelForm):
@@ -43,3 +40,18 @@ class MessageUpdateForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['recipient', 'content', 'incident_date', 'user_devices']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+
+        super(MessageUpdateForm, self).__init__(*args, **kwargs)
+
+        self.fields['recipient'].queryset = User.objects.all().exclude(
+            is_staff=False)
+        self.fields['incident_date'].widget = DateTimePickerInput(options={
+            "format": "DD.MM.YYYY HH:mm",
+            "locale": "de",
+        }, )
+        self.fields['user_devices'].queryset = DeviceUserAssignment.objects.all().filter(
+            assigned_user__user=self.request.user
+        )
